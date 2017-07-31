c575ed294180ec000abd08e1d6fb3e9d9db5dd1f
Jake Westerfield <jecobwesterfield@gmail.com>
2017-07-31 16:53:20 -0700

    Added wish.cs model
    -Added wish.cs model

---
+++ WebApplication1/WebApplication1/Models/wish.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class wish
+    {
+        public string Location { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Activity { get; set; }
+
+        public string Image { get; set;}
+    }
+}
